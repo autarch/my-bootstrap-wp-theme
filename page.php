b1765face157c1a3581cd9992fb9c5579213b729
Dave Rolsky <autarch@urth.org>
2012-12-01 11:43:39 -0600

    Use full width template as default page

--- page.php
+++ page.php
@@ -15,24 +15,22 @@
 
 get_header(); ?>
 
-<div id="primary" class="span8">
+<section id="primary" class="span12">
 	<?php tha_content_before(); ?>
 	<div id="content" role="main">
 		<?php tha_content_top();
 		
 		the_post();
 		get_template_part( '/partials/content', 'page' );
-		comments_template();
-
+		comments_template( '', true );
+		
 		tha_content_bottom(); ?>
 	</div><!-- #content -->
 	<?php tha_content_after(); ?>
-</div><!-- #primary -->
+</section><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
-
 
 /* End of file page.php */
 /* Location: ./wp-content/themes/the-bootstrap/page.php */
