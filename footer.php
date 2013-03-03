efccbc4dd1a1e476f2fbd167e82ec7a04df9b748
Dave Rolsky <autarch@urth.org>
2013-03-03 10:34:04 -0600

    Starting on exploreveg theme
    Implemented header & footer

--- footer.php
+++ footer.php
@@ -5,36 +5,45 @@
  * @package		The Bootstrap
  * @since		1.0.0	- 05.02.2012
  */
+?>
 
-				tha_footer_before(); ?>
-				<footer id="colophon" role="contentinfo" class="span12">
-					<?php tha_footer_top(); ?>
-					<div id="page-footer" class="well clearfix">
-						<?php wp_nav_menu( array(
-							'container'			=>	'nav',
-							'container_class'	=>	'subnav',
-							'theme_location'	=>	'footer-menu',
-							'menu_class'		=>	'credits nav nav-pills pull-left',
-							'depth'				=>	3,
-							'fallback_cb'		=>	'the_bootstrap_credits',
-							'walker'			=>	new The_Bootstrap_Nav_Walker,
-						) );
-						?>
-						<div id="site-generator"<?php echo has_nav_menu('footer-menu') ? ' class="footer-nav-menu"' : ''; ?>>
-							<a	href="<?php echo esc_url( __( 'http://wordpress.org/', 'the-bootstrap' ) ); ?>"
-								title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'the-bootstrap' ); ?>"
-								target="_blank"
-								rel="generator"><?php printf( _x( 'Proudly powered by %s', 'WordPress', 'the-bootstrap' ), 'WordPress' ); ?></a>
-						</div>
-					</div><!-- #page-footer .well .clearfix -->
-					<?php tha_footer_bottom(); ?>
-				</footer><!-- #colophon -->
-				<?php tha_footer_after(); ?>
-			</div><!-- #page -->
-		</div><!-- .container -->
+    </div>
+    <?php tha_footer_before(); ?>
+    <div class="container footer">
+      <footer>
+        <ul id="footer-nav">
+          <?php wp_nav_menu( array(
+              'container'         =>  'nav',
+              'container_class'   =>  'subnav',
+              'theme_location'    =>  'footer-menu',
+              'depth'             =>  3,
+              'fallback_cb'       =>  false,
+              'walker'            =>  new The_Bootstrap_Nav_Walker,
+          ) );
+          ?>
+          <li><a href="about.html">About CAA</a></li>
+          <li><a href="contact.html">Contact Us</a></li>
+          <li><a href="privacy.html">Privacy Policy</a></li>
+        </ul>
+
+        <p>
+          <strong>Email:</strong> <a href="mailto:info@exploreveg.org">info@exploreveg.org</a>
+          <br>
+          <strong>Phone:</strong> 612-276-2242
+        </p>
+
+        <p>
+          <a href="http://creativecommons.org/licenses/by-sa/3.0/us/"><img
+              src="<?php echo bloginfo('stylesheet_directory'); ?>/img/by-sa.png" width="67" height="13"></a>
+          Copyright &copy; 2012-<?php print(Date("Y")); ?> Compassionate Action for
+          Animals. <a href="http://creativecommons.org/licenses/by-sa/3.0/us/">Some
+          rights reserved</a>.
+        </p>
+      </footer>
+    </div>
 	<!-- <?php printf( __( '%d queries. %s seconds.', 'the-bootstrap' ), get_num_queries(), timer_stop(0, 3) ); ?> -->
 	<?php wp_footer(); ?>
-	</body>
+  </body>
 </html>
 <?php
 
