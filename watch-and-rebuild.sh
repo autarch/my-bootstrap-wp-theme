b63fa0089a382b22e6563a4d70f4724f73e08642
Dave Rolsky <autarch@urth.org>
2013-03-17 16:45:19 -0500

    Rebuild when JS changes

--- watch-and-rebuild.sh
+++ watch-and-rebuild.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
 while [ true ]; do
-    inotifywait -e modify -e create -e delete -e move --exclude '.#*' --exclude '#*#' -r less
+    inotifywait -e modify -e create -e delete -e move --exclude '.#*' --exclude '#*#' -r less js
     ./regen-theme.sh
 done
