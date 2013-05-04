5156fdbd61fc5fc67e4f36b0a91d806de4081ed9
Dave Rolsky <autarch@urth.org>
2013-05-04 10:20:58 -0500

    Add social share buttons to blog posts

--- partials/content-single.php
+++ partials/content-single.php
@@ -24,6 +24,37 @@ tha_entry_before(); ?>
 		the_bootstrap_link_pages(); ?>
 	</div><!-- .entry-content -->
 
+    <div id="social-buttons row">
+      <div class="span2">
+        <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+          fjs.parentNode.insertBefore(js, fjs);
+          }(document, 'script', 'facebook-jssdk'));</script>
+        <div class="fb-like" data-send="false" data-layout="button_count" data-width="100" data-show-faces="false"></div>
+      </div>
+
+      <div class="span2">
+        <a href="https://twitter.com/share" class="twitter-share-button social-button" data-via="exploreveg">Tweet</a>
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+      </div>
+
+      <div class="span2">
+        <!-- Place this tag where you want the +1 button to render. -->
+        <div class="g-plusone social-button"></div>
+        <!-- Place this tag after the last +1 button tag. -->
+        <script type="text/javascript">
+          (function() {
+          var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+          po.src = 'https://apis.google.com/js/plusone.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+          })();
+        </script>
+      </div>
+    </div>
+
 	<?php tha_entry_bottom(); ?>
 </article><!-- #post-<?php the_ID(); ?> -->
 <?php tha_entry_after();
