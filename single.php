4b11eba784ff01da4ff8f3bb4d36799f16a49516
Dave Rolsky <autarch@urth.org>
2013-05-11 14:48:10 -0500

    Fix single blog post display

--- single.php
+++ single.php
@@ -10,37 +10,37 @@
 
 get_header(); ?>
 
-<section id="primary" class="span8">
-	
-	<?php tha_content_before(); ?>
-	<div id="content" role="main">
-		<?php tha_content_top();
+      <div class="row">
+        <div class="span9">
+          <?php tha_content_before(); ?>
+          <?php tha_content_top();
 
-		while ( have_posts() ) {
-			the_post();
-			get_template_part( '/partials/content', 'single' );
-		} ?>
+            while ( have_posts() ) {
+                the_post();
+                get_template_part( '/partials/content', 'single' );
+            } ?>
 		
-        <h3 class="assistive-text"><?php _e( 'Post navigation', 'the-bootstrap' ); ?></h3>
-        <div class="row">
-		  <div class="span4">
-            <span class="next"><?php next_post_link( '%link', sprintf( '%1$s&nbsp;&rarr;', __( 'Next Post', 'the-bootstrap' ) ) ); ?></span>
-          </div>
+            <h3 class="assistive-text"><?php _e( 'Post navigation', 'the-bootstrap' ); ?></h3>
 
-		  <div class="span4">
-		    <span class="previous"><?php previous_post_link( '%link', sprintf( '&larr;&nbsp;%1$s', __( 'Previous Post', 'the-bootstrap' ) ) ); ?></span>
-          </div>
+            <div class="row">
+		      <div class="span4">
+		        <span class="previous"><?php previous_post_link( '%link', sprintf( '&larr;&nbsp;%1$s', __( 'Previous Post', 'the-bootstrap' ) ) ); ?></span>
+              </div>
+
+		      <div class="span4">
+                <span class="next"><?php next_post_link( '%link', sprintf( '%1$s&nbsp;&rarr;', __( 'Next Post', 'the-bootstrap' ) ) ); ?></span>
+              </div>
+            </div>
+		
+          <?php tha_content_bottom(); ?>
         </div>
-		
-		<?php tha_content_bottom(); ?>
-	</div><!-- #content -->
-	<?php tha_content_after(); ?>
-</section><!-- #primary -->
+        <?php
+           tha_content_after();
+           get_sidebar(); ?>
+      </div>
 
 <?php
-get_sidebar();
 get_footer();
 
-
-/* End of file index.php */
+/* End of file single.php */
 /* Location: ./wp-content/themes/the-bootstrap/single.php */
