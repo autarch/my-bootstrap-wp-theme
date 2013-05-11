e231efa2c30138f1a9e667c5aef6a2da8d168733
Dave Rolsky <autarch@urth.org>
2013-05-11 14:50:47 -0500

    Fix news display
    Remove page title top margin

--- templates/news.php
+++ templates/news.php
@@ -9,50 +9,47 @@
 
 get_header(); ?>
 
-<section id="primary" class="span8">
+      <div class="row">
+        <div class="span9">
+          <?php tha_content_before(); ?>
+          <?php tha_content_top(); ?>
 
-	<?php tha_content_before(); ?>
-	<div id="content" role="main">
-		<?php tha_content_top(); ?>
+          <header class="page-header">
+            <h1 class="page-title"><?php the_title(); ?></h1>
+		  </header><!-- .page-header -->
 
-        <header class="page-header">
-          <h1 class="page-title"><?php the_title(); ?></h1>
-		</header><!-- .page-header -->
+          <?php
+          $temp_query = clone $wp_query;
 
-		<?php
-        $temp_query = clone $wp_query;
+          $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+          $wp_query = new WP_Query(
+              array(
+                 'post_type'      => 'post',
+                 'posts_per_page' => 10,
+                 'paged'          => $paged,
+              )
+          );
 
-        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-        $wp_query = new WP_Query(
-            array(
-               'post_type'      => 'post',
-               'posts_per_page' => 10,
-               'paged'          => $paged,
-            )
-        );
+          if ( have_posts() ) {
+              while ( have_posts() ) {
+                 the_post();
+                  get_template_part( '/partials/content', get_post_format() );
+              }
+              the_bootstrap_content_nav();
+          } else {
+              get_template_part( '/partials/content', 'not-found' );
+          }
 
-        if ( have_posts() ) {
-			while ( have_posts() ) {
-               the_post();
-				get_template_part( '/partials/content', get_post_format() );
-			}
-			the_bootstrap_content_nav();
-		} else {
-			get_template_part( '/partials/content', 'not-found' );
-		}
-        ?>
+          $wp_query = clone $temp_query;
 
+          tha_content_bottom(); ?>
+        </div>
         <?php
-
-        $wp_query = clone $temp_query;
-
-		tha_content_bottom(); ?>
-	</div><!-- #content -->
-	<?php tha_content_after(); ?>
-</section><!-- #primary -->
+           tha_content_after();
+           get_sidebar(); ?>
+      </div>
 
 <?php
-get_sidebar();
 get_footer();
 
 /* End of file news.php */
