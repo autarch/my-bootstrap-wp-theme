8544b682e528dc9fe34853545745963272f22bc4
Dave Rolsky <autarch@urth.org>
2013-05-11 14:21:56 -0500

    Restyle footer based on Cali's design

--- footer.php
+++ footer.php
@@ -7,43 +7,41 @@
  */
 ?>
 
-    </div>
-    <?php tha_footer_before(); ?>
-    <div class="container footer">
+      <?php tha_footer_before(); ?>
       <footer>
-        <ul id="footer-nav">
-          <?php wp_nav_menu( array(
-              'container'         =>  'nav',
-              'container_class'   =>  'subnav',
-              'theme_location'    =>  'footer-menu',
-              'depth'             =>  3,
-              'fallback_cb'       =>  false,
-              'walker'            =>  new The_Bootstrap_Nav_Walker,
-          ) );
-          ?>
-          <li><a href="about.html">About CAA</a></li>
-          <li><a href="contact.html">Contact Us</a></li>
-          <li><a href="privacy.html">Privacy Policy</a></li>
-        </ul>
+        <div id="footer-nav">
+          <ul>
+            <?php wp_nav_menu( array(
+                'theme_location'    =>  'footer-menu',
+                'depth'             =>  3,
+                'fallback_cb'       =>  false,
+                'walker'            =>  new The_Bootstrap_Nav_Walker,
+            ) );
+            ?>
+          </ul>
+        </div>
 
-        <p>
-          <strong>Email:</strong> <a href="mailto:info@exploreveg.org">info@exploreveg.org</a>
-          <br>
-          <strong>Phone:</strong> 612-276-2242
-        </p>
+        <div id="colophon">
+          <p>
+            <strong>Email:</strong> <a href="mailto:info@exploreveg.org">info@exploreveg.org</a>
+            <br>
+            <strong>Phone:</strong> 612-276-2242
+          </p>
 
-        <p>
-          <a href="http://creativecommons.org/licenses/by-sa/3.0/us/"
-             ><img src="<?php echo bloginfo('stylesheet_directory'); ?>/img/by-sa.png"
-                   alt="Creative Commons BY-SA logo" width="67" height="13"></a>
-          Copyright &copy; 2012-<?php print(Date("Y")); ?> Compassionate Action for
-          Animals. <a href="http://creativecommons.org/licenses/by-sa/3.0/us/">Some
-          rights reserved</a>.
-        </p>
+          <p>
+            <a href="http://creativecommons.org/licenses/by-sa/3.0/us/"
+               ><img src="<?php echo bloginfo('stylesheet_directory'); ?>/img/by-sa.png"
+                     alt="Creative Commons BY-SA logo" width="67" height="13"></a>
+            Copyright &copy; 2012-<?php print(Date("Y")); ?> Compassionate Action for
+            Animals. <a href="http://creativecommons.org/licenses/by-sa/3.0/us/">Some
+              rights reserved</a>.
+          </p>
+        </div>
       </footer>
+      <!-- <?php printf( __( '%d queries. %s seconds.', 'the-bootstrap' ), get_num_queries(), timer_stop(0, 3) ); ?> -->
+      <?php wp_footer(); ?>
+
     </div>
-	<!-- <?php printf( __( '%d queries. %s seconds.', 'the-bootstrap' ), get_num_queries(), timer_stop(0, 3) ); ?> -->
-	<?php wp_footer(); ?>
   </body>
 </html>
 <?php
