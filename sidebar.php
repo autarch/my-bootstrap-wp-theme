a275b5ad3cbd21a96c1c883bc5e5435f1e6444c1
Dave Rolsky <autarch@urth.org>
2013-05-11 13:55:32 -0500

    Implement sidebar according to Cali's design

--- sidebar.php
+++ sidebar.php
@@ -9,78 +9,74 @@
 tha_sidebars_before(); ?>
 
         <div class="span3">
-	      <?php tha_sidebar_top(); ?>
-          <aside
-            <?php if ( is_front_page() ) : echo 'id="front-page-actions" '; endif ?>
-            class="right-side-actions">
-            <div class="well">
-              <p>
-                News and events:
-              </p>
+          <div id="right-nav">
+	        <?php tha_sidebar_top(); ?>
+            <div
+               <?php if ( is_front_page() ) : echo 'id="front-page-actions" '; endif ?>
+               class="right-side-actions">
 
-              <div id="announce-subscribe-form">
-              <?php echo do_shortcode( '[contact-form-7 id="150" title="Announce Subscribe"]' ); ?>
+              <h2 class="first">Get Involved</h2>
+
+              <div class="right-side-content">
+                <div id="announce-subscribe-form">
+                  <?php echo do_shortcode( '[contact-form-7 id="150" title="Announce Subscribe"]' ); ?>
+                </div>
+
+                <p>
+                  <a href="/donate" class="btn btn-primary" title="Support our work">Donate</a>
+                </p>
+
+                <p>
+                  <a href="/resources/vsk" class="btn btn-primary" title="Order a free Vegetarian Starter Kit">Free Go Veg Kit</a>
+                </p>
+
+                <p>
+                  <a href="/volunteer" class="btn btn-primary" title="Make it all happen">Volunteer</a>
+                </p>
               </div>
-              <p>
-                Help us help animals:
-                <a href="/donate" class="btn btn-primary">Donate</a>
-              </p>
-
-              <p>
-                Free Veg Starter Kit:
-                <a href="/resources/vsk" class="btn btn-primary">Order</a>
-              </p>
-
-              <p>
-                Get involved:
-                <a href="/volunteer" class="btn btn-primary">Volunteer</a>
-              </p>
-            </div>            
-          </aside>
-
-          <div id="announce-subscribe-response" class="modal hide fade">
-            <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-              <h3 id="announce-subscribe-response-header"></h3>
             </div>
-            <div class="modal-body">
-              <p id="announce-subscribe-response-text"></p>
-            </div>
-            <div class="modal-footer">
-              <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-            </div>
-          </div>
 
-          <div id="events">
+            <div id="announce-subscribe-response" class="modal hide fade">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h3 id="announce-subscribe-response-header"></h3>
+              </div>
+              <div class="modal-body">
+                <p id="announce-subscribe-response-text"></p>
+              </div>
+              <div class="modal-footer">
+                <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+              </div>
+            </div>
+
             <h2>
-              Upcoming Events
-              <a class="pull-right" href="#" title="Upcoming Events Feed"
+              <a href="#" title="See all upcoming events">Upcoming Events</a>
+              <a href="#" title="Upcoming events feed"
                  ><img class="rss"
                        src="<?php echo bloginfo('stylesheet_directory'); ?>/img/rss.png"
                        alt="RSS icon" height="16" width="16"></a>
             </h2>
 
-            <div class="event">
-              <h3><a href="#">Holiday Cooking Class</a></h3>
+            <div class="right-side-content">
+              <div class="event">
+                <h3 class="date">Friday, Dec 17, 2012</h3>
+                <h3 class="event"><a href="#">Holiday Cooking Class</a></h3>
+              </div>
 
-              <p class="date">Dec 17, 2012</p>
+              <div class="event">
+                <h3 class="date">Wednesday, Dec 21, 2012</h3>
+                <h3 class="event"><a href="#">Dine Out at Bad Waitress Diner</a></h3>
+              </div>
+
+              <div class="event">
+                <h3 class="date">Thursday, Apr 04, 2013</h3>
+                <h3 class="event"><a href="#">Annual Banquet</a></h3>
+              </div>
             </div>
 
-            <div class="event">
-              <h3><a href="#">Dine Out at Bad Waitress Diner</a></h3>
-
-              <p class="date">Dec 21, 2012</p>
-            </div>
-
-            <div class="event">
-              <h3><a href="#">Annual Banquet</a></h3>
-
-              <p class="date">Apr 04, 2013</p>
-            </div>
+	        <?php tha_sidebar_bottom(); ?>
           </div>
-
-	      <?php tha_sidebar_bottom(); ?>
-        </div>
+       </div>
 
 <?php tha_sidebars_after();
 
