71035bbaabc354192efd03a9abc1d135f1ca613e
Dave Rolsky <autarch@urth.org>
2013-05-26 15:36:14 -0500

    First stab at TLOV theme

--- footer.php
+++ footer.php
@@ -12,13 +12,11 @@
 					<div id="page-footer" class="clearfix">
                         <ul id="footer-nav">
                             <li><a href="mailto:info@tcvegfest.com">Contact Us</a></li>
-                            <li><a href="/about-the-festival/">About Us</a></li>
-                            <li><a href="/code-of-conduct/">Code of Conduct</a></li>
-                            <li><a href="/credits/">Credits</a></li>
+                            <li><a href="http://2013.tcvegfest.com/code-of-conduct/">Code of Conduct</a></li>
                         </ul>
 
                         <a href="http://creativecommons.org/licenses/by-sa/3.0/us/"><img width="67" height="13" src="<?php echo bloginfo('stylesheet_directory'); ?>/img/cc-by-sa.png" title="Creative Commons BY-SA" alt="Creative Commons BY-SA logo"></a>
-                        Copyright &copy; 2011-2013 <a href="http://www.exploreveg.org">Compassionate Action for Animals</a><br /><a href="http://creativecommons.org/licenses/by-sa/3.0/us/" title="Creative Commons BY-SA">Some rights reserved</a>
+                        Copyright &copy; 2013 <a href="http://www.exploreveg.org">Compassionate Action for Animals</a><br /><a href="http://creativecommons.org/licenses/by-sa/3.0/us/" title="Creative Commons BY-SA">Some rights reserved</a>
 
                         <div id="caa-logo">
                                              <a href="http://www.exploreveg.org" title="Compassionate Action for Animals"><img width="100" height="60" src="<?php echo bloginfo('stylesheet_directory'); ?>/img/caa-logo.png" alt="Compassionate Action for Animals logo"></a>
--- header.php
+++ header.php
@@ -41,24 +41,22 @@
 					) ); ?>
                     <?php if ( is_front_page() ) : ?>
                       <div id="front-page-logo">
-                        <img src="<?php echo bloginfo('stylesheet_directory'); ?>/img/vegfest-logo-300x269.png" width="225" height="202">
+                        <img src="<?php echo bloginfo('stylesheet_directory'); ?>/img/tlov-logo-316x138.png" width="316" height="138">
                       </div>
                     <?php else : ?>
                       <a  id="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-                        <img src="<?php echo bloginfo('stylesheet_directory'); ?>/img/vegfest-logo-167x150.png" width="167" height="150">
+                        <img src="<?php echo bloginfo('stylesheet_directory'); ?>/img/tlov-logo-172x75.png" width="172" height="75">
                       </a>
                     <?php endif; ?>
                     </a>
                     <?php if ( ! is_front_page() ) : ?>
                     <div id="titles">
-					    <hgroup>
-                            <h1 id="site-title">
-                                <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-                                    <span><?php bloginfo( 'name' ); ?></span>
-                                </a>
-                            </h1>
-                            <h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
-					    </hgroup>
+                      <h1 id="site-title">
+                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+                          <span><?php bloginfo( 'name' ); ?></span>
+                        </a>
+                      </h1>
+                      <h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
                     </div>
                     <?php endif; ?>
 					
