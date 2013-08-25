20c31d3274309b5d30dc79f92daa537e10f2227e
Dave Rolsky <autarch@urth.org>
2013-08-25 11:05:16 -0500

    Make pods template work within constraints of Pods

--- templates/pods.php
+++ templates/pods.php
@@ -9,16 +9,6 @@ get_header(); ?>
         <div class="span9">
           <?php tha_content_before(); ?>
           <?php tha_content_top(); ?>
-
-          <header class="page-header">
-              <h1 class="page-title">
-              <?php
-                  # This is pretty gross but AFAICT there's no other way to get the pods page title!
-                  $pod_page = pod_page_exists(); echo $pod_page['title'];
-               ?>
-               </h1>
-		  </header><!-- .page-header -->
-
           <?php pods_content(); ?>
         </div>
         <?php
