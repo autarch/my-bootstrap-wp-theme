93affd1cdf9ee6e345d97fe4dea3e6cf1e2b59e4
Dave Rolsky <autarch@urth.org>
2013-11-10 17:32:10 +0000

    Show featured image on pages

--- partials/content-page.php
+++ partials/content-page.php
@@ -18,7 +18,11 @@ tha_entry_before(); ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content clearfix">
-		<?php
+        <?php if ( has_post_thumbnail() ) : ?>
+        <a class="thumbnail post-thumbnail pull-right span2" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+             <?php the_post_thumbnail( 'medium' ); ?>
+        </a>
+        <?php endif;
 		the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'the-bootstrap' ) );
 		the_bootstrap_link_pages(); ?>
 	</div><!-- .entry-content -->
