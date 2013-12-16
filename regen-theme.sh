5a45449c4ec1cf284463c4b41115cce9d2467a3c
Dave Rolsky <autarch@urth.org>
2013-12-15 20:39:44 -0600

    Make sure to regen the-bootstrap.js script

--- regen-theme.sh
+++ regen-theme.sh
@@ -5,6 +5,8 @@ cp ./less/bootstrap/img/* img/
 
 cat less/bootstrap/js/bootstrap-transition.js less/bootstrap/js/bootstrap-alert.js less/bootstrap/js/bootstrap-button.js less/bootstrap/js/bootstrap-carousel.js less/bootstrap/js/bootstrap-collapse.js less/bootstrap/js/bootstrap-dropdown.js less/bootstrap/js/bootstrap-modal.js less/bootstrap/js/bootstrap-tooltip.js less/bootstrap/js/bootstrap-popover.js less/bootstrap/js/bootstrap-scrollspy.js less/bootstrap/js/bootstrap-tab.js less/bootstrap/js/bootstrap-typeahead.js less/bootstrap/js/bootstrap-affix.js js/jquery.cookie.js js/exploreveg.js > js/bootstrap.js
 
-./node_modules/uglify-js/bin/uglifyjs -nc -v js/bootstrap.js > js/bootstrap.min.tmp.js
+for js in js/bootstrap js/the-bootstrap; do
+    ./node_modules/uglify-js/bin/uglifyjs -nc -v "$js".js > "$js".tmp.min.js
+    mv $js.tmp.min.js $js.min.js
+done
 
-mv js/bootstrap.min.tmp.js js/bootstrap.min.js
