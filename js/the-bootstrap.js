11903f12675437414f71a5810106f12282aec85a
Dave Rolsky <autarch@urth.org>
2013-12-15 20:41:39 -0600

    Only add auto table styles to table-auto class tables

--- js/the-bootstrap.js
+++ js/the-bootstrap.js
@@ -1,5 +1,5 @@
 jQuery(function($){
-	$( 'table' ).addClass( 'table table-striped table-hover' );
+	$( 'table.table-auto' ).addClass( 'table table-striped table-hover' );
 	$( '#submit' ).addClass( 'btn btn-primary btn-large' );
 	$( '#wp-calendar' ).addClass( 'table table-striped table-bordered' );
 	
