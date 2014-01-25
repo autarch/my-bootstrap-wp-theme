40b945f027a3910ef455ee5d1936c696d894f72d
Dave Rolsky <autarch@urth.org>
2014-01-25 13:16:44 -0600

    No need to minify JS from CLI

--- regen-theme.sh
+++ regen-theme.sh
@@ -17,8 +17,3 @@ cat bootswatch/bower_components/bootstrap/dist/js/bootstrap.js \
     js/exploreveg.js \
     > js/compiled-js.js
 
-for js in compiled-js html5shiv respond; do
-    ./node_modules/.bin/uglifyjs -nc -v js/$js.js > js/$js.tmp.min.js
-    mv js/$js.tmp.min.js js/$js.min.js
-done
-
