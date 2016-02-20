ff83ce4dc63cf80a804198aeb0f77c16290c6711
Dave Rolsky <autarch@urth.org>
2016-02-20 07:45:08 -0600

    Add addthis to header

--- header.php
+++ header.php
@@ -40,6 +40,8 @@ $favicon = $logo . '-favicon.ico';
         <?php tha_head_bottom(); ?>
         <?php wp_head(); ?>
 
+        <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-56be203d5f1e147b"></script>
+
     </head>
 
     <body <?php body_class(); ?>>
