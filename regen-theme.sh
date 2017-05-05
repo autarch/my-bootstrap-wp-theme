351b8446a9a3f9dbeb71476a92a179141013951d
Dave Rolsky <autarch@urth.org>
2017-05-05 17:38:45 -0500

    Fix regen-theme.sh

--- regen-theme.sh
+++ regen-theme.sh
@@ -16,14 +16,3 @@ cat bootswatch/bower_components/bootstrap/dist/js/bootstrap.js \
     js/the-bootstrap.js \
     js/exploreveg.js \
     > js/compiled-js.js
-
-if [[ $(basename `pwd`) == "exploreveg-test" ]]; then
-    SITE_ID=11
-else
-    SITE_ID=10
-fi
-
-CACHE_DIR=/var/www/admin.exploreveg.org/wp-content/cache/minify/0000${SITE_ID}
-
-sudo rm $CACHE_DIR/*.js*
-sudo rm $CACHE_DIR/*.css*
